16fab60dc65a592be631bd56328567e69bbd92f7
pawel.brzezinski <pawe.a.brzezinski@capgemini.com>
2022-09-30 12:28:53 +0200

    Add and display simple players dummy data

---
+++ screens/PlayersScreen.js
@@ -0,0 +1,20 @@
+import { FlatList } from "react-native";
+import PlayerGridTile from "../components/PlayerGridTile";
+
+import { PLAYERS_DUMMY_DATA } from "../data/data"
+
+const PlayersScreen = () => {
+  const renderPlayerItem = (playerData) => {
+    return <PlayerGridTile name={playerData.item.name} />;
+  };
+
+  return (
+    <FlatList
+      data={PLAYERS_DUMMY_DATA}
+      keyExtractor={(item) => item.id}
+      renderItem={renderPlayerItem}
+    />
+  );
+};
+
+export default PlayersScreen;
