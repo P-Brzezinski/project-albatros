d178e80b98495e98ae8f67f6790c36b25d98768f
P-Brzezinski <58233105+P-Brzezinski@users.noreply.github.com>
2022-10-05 17:48:28 +0200

    Revert "Revert 4 add new player"

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import PlayersScreen from "./screens/PlayersScreen";
+import PlayerDetails from "./components/player/PlayerDetails";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="dark" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="PlayersScreen" component={PlayersScreen} />
+          <Stack.Screen name="PlayerDetails" component={PlayerDetails} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ data/data.js
@@ -0,0 +1,10 @@
+import Player from '../models/player'
+
+export const PLAYERS_DUMMY_DATA = [
+  new Player("p1", "Player 1"),
+  new Player("p2", "Player 2"),
+  new Player("p3", "Player 3"),
+  new Player("p4", "Player 4"),
+  new Player("p5", "Player 5"),
+  new Player("p6", "Player 6"),
+];
