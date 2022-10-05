d8250632cdc9df164000a88e63d3ecf7bbd688af
P-Brzezinski <58233105+P-Brzezinski@users.noreply.github.com>
2022-10-05 17:28:09 +0200

    Revert "Add NewPlayer Screen and side navigation"

--- constants/styles.js
+++
@@ -1,9 +0,0 @@
-export const GlobalStyles = {
-  colors: {
-    primaryWhite: "white",
-    primaryBlack: "black",
-    primaryLight: "lightblue",
-    primaryMedium: "#137fae",
-    primaryRipple: "white",
-  },
-};
