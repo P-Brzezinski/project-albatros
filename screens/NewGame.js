b0da01d4d6d26862471e69d28eb9f5c54e503757
pawel.brzezinski <pawe.a.brzezinski@capgemini.com>
2022-10-07 10:17:08 +0200

    Add picked players context to pass ids to new game

--- screens/NewGame.js
+++ screens/NewGame.js
@@ -1,11 +1,16 @@
+import { useContext } from "react";
 import { Text, View } from "react-native"
+import { PickedPlayersContext } from "../store/picked-players-context";
 
 const NewGame = () => {
+  const ctx = useContext(PickedPlayersContext);
+
   return (
     <View>
       <Text>New Game Screen</Text>
+      <Text>Picked players ids: {ctx.pickedPlayersIds}</Text>
     </View>
   );
 };
 
-export default NewGame
+export default NewGame;
